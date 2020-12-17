38c4ba199f30a4cd6f77b24a19b7aa83395fcc59
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-17 14:00:15 +0000

    done

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 #Write your code below this row 👇
-
+nums = 0 
+for nums in range(0,101):
+  print(nums)
+  if  nums % 3 == 0 and nums % 5 ==0:
+    print("fizzbuzz")
+  if nums  % 5 == 0: 
+   print("buzz")
+  if nums % 3 == 0:
+     print("fizzbuzz")
